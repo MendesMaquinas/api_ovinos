ca8642cf8007d52a2baeb009437737a951d0c32b
MendesMaquinas <api.mendes@mendesmaquinas.com.br>
2023-12-04 10:45:11 -0300

    up

--- src/routes/OvinosRoutes.js
+++ src/routes/OvinosRoutes.js
@@ -2,9 +2,10 @@ const express = require("express");
 const router = express.Router();
 
 const controller = require("../controllers/ovinos.controllers");
+const validators = require("../validators/ovinos.validator");
 
 router.get("/", controller.findAll);
-router.get("/:id", controller.findByPk);
+router.get("/:id", validators.findByPk(), controller.findByPk);
 router.post("/", controller.create);
 router.put("/", controller.update);
 router.delete("/:id", controller.deleta);
---
+++ src/utils/errorMessage.js
@@ -0,0 +1,13 @@
+const validatorMessage = function (atributo) {
+    return `A propriedade [${atributo}] é inválida.`;
+  };
+  
+  const notExists = function (atributo) {
+    return `[${atributo}] não existe.`;
+  };
+  
+  module.exports = {
+    validatorMessage,
+    notExists,
+  };
+  
